4d70d497fe1923b48e2d1f0cd62877e4aae5964a
RagnarUtstyr <ragnarboe88@gmail.com>
2024-10-21 15:28:09 +0200

    eq

--- key.js
+++ key.js
@@ -1,7 +1,24 @@
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
 
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD_4kINWig7n6YqB11yM2M-EuxGNz5uekI",
+    authDomain: "roll202-c0b0d.firebaseapp.com",
+    databaseURL: "https://roll202-c0b0d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "roll202-c0b0d",
+    storageBucket: "roll202-c0b0d.appspot.com",
+    messagingSenderId: "607661730400",
+    appId: "1:607661730400:web:b4b3f97a12cfae373e7105",
+    measurementId: "G-6X5L39W56C"
+};
+
+// Initialize Firebase App
+const app = initializeApp(firebaseConfig);
+
 // Initialize Firebase Database
-const db = getDatabase();
+const db = getDatabase(app);
 
 // Function to create or join a room and store the key in cookies
 function handleInviteKeySubmit(pageType) {
